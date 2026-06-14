b1f492cc3c8613cd771744a8ff4dd3000f6d2517
Ömer Halis DEMİR <omerhd16@outlook.com>
2026-06-14 20:00:45 +0300

    components/pages/service-detail/index.ts eklendi

---
+++ components/pages/service-detail/index.ts
@@ -0,0 +1,7 @@
+export { ServiceHero } from "./ServiceHero"
+export { ServiceFeatures } from "./ServiceFeatures"
+export { ServiceProcess } from "./ServiceProcess"
+export { ServiceTech } from "./ServiceTech"
+export { ServiceProjects } from "./ServiceProjects"
+export { ServiceFAQ } from "./ServiceFAQ"
+export { ServiceCTA } from "./ServiceCTA"
