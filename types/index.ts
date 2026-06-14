572a3409e3dd7c493c8796a932afb7986f7d60e7
Ömer Halis DEMİR <omerhd16@outlook.com>
2026-06-14 20:01:04 +0300

    index.ts tip tanımı eklendi

---
+++ types/index.ts
@@ -0,0 +1,4 @@
+export type { Project, ProjectCategory } from "./project"
+export type { Service } from "./service"
+export type { Post } from "./post"
+export type { Testimonial } from "./testimonial"
