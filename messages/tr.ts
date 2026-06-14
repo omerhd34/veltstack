58d827c44d1bce6007d1c284d945dfe10f0c1787
Ömer Halis DEMİR <omerhd16@outlook.com>
2026-06-14 20:01:01 +0300

    Global mesajlar tr.ts eklendi

---
+++ messages/tr.ts
@@ -0,0 +1,41 @@
+import { navTr } from "@/components/layout/navbar/messages/tr";
+import { footerTr } from "@/components/layout/footer/messages/tr";
+import { heroHomeTr } from "@/components/sections/hero/messages/tr";
+import { servicesHomeTr } from "@/components/sections/services/messages/tr";
+import { servicesMarqueeHomeTr } from "@/components/sections/services-marquee/messages/tr";
+import { whoWeAreHomeTr } from "@/components/sections/who-we-are/messages/tr";
+import { whyMeHomeTr } from "@/components/sections/why-me/messages/tr";
+import { processHomeTr } from "@/components/sections/process/messages/tr";
+import { projectsHomeTr } from "@/components/sections/projects/messages/tr";
+import { testimonialsHomeTr } from "@/components/sections/testimonials/messages/tr";
+import { blogPreviewHomeTr } from "@/components/sections/blog-preview/messages/tr";
+import { ctaHomeTr } from "@/components/sections/cta/messages/tr";
+import { clientLogosHomeTr } from "@/components/sections/client-logos/messages/tr";
+import { projectDetailsTr } from "@/components/pages/project-detail/messages/tr";
+import { pagesTr } from "@/app/[locale]/messages/pages/tr";
+import { contactTr } from "@/app/[locale]/iletisim/messages/tr";
+import { aboutTr } from "@/app/[locale]/hakkimda/messages/tr";
+
+const messages = {
+  nav: navTr,
+  footer: footerTr,
+  home: {
+    ...heroHomeTr,
+    ...servicesHomeTr,
+    ...servicesMarqueeHomeTr,
+    ...whoWeAreHomeTr,
+    ...whyMeHomeTr,
+    ...processHomeTr,
+    ...projectsHomeTr,
+    ...testimonialsHomeTr,
+    ...blogPreviewHomeTr,
+    ...ctaHomeTr,
+    ...clientLogosHomeTr,
+  },
+  pages: pagesTr,
+  projectDetails: projectDetailsTr,
+  contact: contactTr,
+  about: aboutTr,
+} as const;
+
+export default messages;
