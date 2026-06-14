610d119254f6826b2bcb873a1259acb9f56ee1a2
Ömer Halis DEMİR <omerhd16@outlook.com>
2026-06-14 20:01:00 +0300

    index.ts eklendi

---
+++ components/ui/shadcn/index.ts
@@ -0,0 +1,28 @@
+export { Button, buttonVariants } from "./button";
+export { Badge, badgeVariants } from "./badge";
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardAction,
+  CardDescription,
+  CardContent,
+} from "./card";
+export {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuContent,
+  NavigationMenuTrigger,
+  NavigationMenuLink,
+  NavigationMenuIndicator,
+  NavigationMenuViewport,
+  navigationMenuTriggerStyle,
+} from "./navigation-menu";
+export {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+} from "./tooltip";
