66ec8c1da84af20c0162d10791067d9eafa00e0a
Ömer Halis DEMİR <omerhd16@outlook.com>
2026-06-14 20:00:48 +0300

    components/sections/cta/messages/en.ts eklendi

---
+++ components/sections/cta/messages/en.ts
@@ -0,0 +1,5 @@
+export const ctaHomeEn = {
+  ctaTitle: "Let's Bring Your Project to Life",
+  ctaSubtitle: "Start your digital transformation with Veltstack today.",
+  ctaButton: "Get in Touch",
+} as const;
