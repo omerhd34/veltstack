a8fdde2763452bae7c3682a9cc63cc4c4fc1724c
Ömer Halis DEMİR <omerhd16@outlook.com>
2026-06-14 20:00:53 +0300

    components/sections/services-marquee/messages/en.ts eklendi

---
+++ components/sections/services-marquee/messages/en.ts
@@ -0,0 +1,12 @@
+export const servicesMarqueeHomeEn = {
+  "servicesMarqueeItems": [
+    "Web Development",
+    "Mobile App Development",
+    "Corporate Website",
+    "E-Commerce",
+    "SEO Services",
+    "Portfolio Website",
+    "API Development",
+    "Maintenance and Support"
+  ]
+} as const
