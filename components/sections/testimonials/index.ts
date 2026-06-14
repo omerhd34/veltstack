41c12eab498bbeae9fc8159125042b3bcbbabbd4
Ömer Halis DEMİR <omerhd16@outlook.com>
2026-06-14 20:00:56 +0300

    components/sections/testimonials/index.ts eklendi

---
+++ components/sections/testimonials/index.ts
@@ -0,0 +1,4 @@
+export { TestimonialsSection } from "./TestimonialsSection"
+export { TestimonialCard } from "./TestimonialCard"
+export { TestimonialSlider } from "./TestimonialSlider"
+export { TestimonialCarousel } from "./TestimonialCarousel"
